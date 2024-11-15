429cbd7542d8925bee363817bf42fc97a40ea656
Tsuev <tsuevjudoka@gmail.com>
2024-11-16 01:31:38 +0300

    update

--- index.js
+++ index.js
@@ -13,54 +13,85 @@ const bot = new TelegramBot(API_KEY_BOT, {
 
 const openai = new OpenAI({ apiKey: API_KEY_OPENAI });
 
+let num = 0;
+let gameIsActive = false;
+
 bot.on("text", async (msg) => {
   try {
-    const regex = /(^|[^a-zA-Z0-9])([Ал1а]бба[зс])($|[^a-zA-Z0-9])/;
+    // const regex = /(^|[^a-zA-Z0-9])([Ал1а]бба[зс])($|[^a-zA-Z0-9])/;
 
-    if (msg.from.id === MAGA && regex.test(msg.text)) {
-      const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "system",
-            content:
-              "Ты участник телеграм чата, тебе написал величайший генерал из игры Command & Conquer: Generals по имени Мага, отдай честь и только потом отвечай на его вопрос и помни что тебя зовут lаббаз, отвечай коротко и в разговорном стиле",
-          },
-          {
-            role: "user",
-            content: msg.text,
-          },
-        ],
-      });
+    // if (msg.from.id === MAGA && regex.test(msg.text)) {
+    //   const completion = await openai.chat.completions.create({
+    //     model: "gpt-3.5-turbo",
+    //     messages: [
+    //       {
+    //         role: "system",
+    //         content:
+    //           "Ты участник телеграм чата, тебе написал величайший генерал из игры Command & Conquer: Generals по имени Мага, отдай честь и только потом отвечай на его вопрос и помни что тебя зовут lаббаз, отвечай коротко и в разговорном стиле",
+    //       },
+    //       {
+    //         role: "user",
+    //         content: msg.text,
+    //       },
+    //     ],
+    //   });
 
-      await bot.sendMessage(chatId, completion.choices[0].message, {
-        reply_to_message_id: msg.message_id,
-      });
+    //   await bot.sendMessage(chatId, completion.choices[0].message, {
+    //     reply_to_message_id: msg.message_id,
+    //   });
 
-      return;
+    //   return;
+    // }
+
+    // if (regex.test(msg.text)) {
+    //   console.log(msg.text);
+
+    //   const completion = await openai.chat.completions.create({
+    //     model: "gpt-3.5-turbo",
+    //     messages: [
+    //       {
+    //         role: "system",
+    //         content:
+    //           "Ты участник телеграм чата, отвечай коротко и в разговорном стиле, помни что тебя зовут lаббаз",
+    //       },
+    //       {
+    //         role: "user",
+    //         content: msg.text,
+    //       },
+    //     ],
+    //   });
+
+    //   await bot.sendMessage(chatId, completion.choices[0].message, {
+    //     reply_to_message_id: msg.message_id,
+    //   });
+    // }
+
+    if ("/game" === msg.text) {
+      gameIsActive = true;
+      num = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+      bot.sendMessage(
+        msg.chat.id,
+        "Я задумал рандомное число от 1 до 100, попробуй его угадать"
+      );
     }
 
-    if (regex.test(msg.text)) {
-      console.log(msg.text);
+    if (gameIsActive && +msg.text) {
+      if (+msg.text > num)
+        bot.sendMessage(msg.chat.id, `Моё число меньше чем ${msg.text}`, {
+          reply_to_message_id: msg.message_id,
+        });
 
-      const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "system",
-            content:
-              "Ты участник телеграм чата, отвечай коротко и в разговорном стиле, помни что тебя зовут lаббаз",
-          },
-          {
-            role: "user",
-            content: msg.text,
-          },
-        ],
-      });
+      if (+msg.text < num)
+        bot.sendMessage(msg.chat.id, `Моё число больше чем ${msg.text}`, {
+          reply_to_message_id: msg.message_id,
+        });
 
-      await bot.sendMessage(chatId, completion.choices[0].message, {
-        reply_to_message_id: msg.message_id,
-      });
+      if (+msg.text === num) {
+        bot.sendMessage(msg.chat.id, `Ты угадал это ${msg.text} 🎆`, {
+          reply_to_message_id: msg.message_id,
+        });
+        gameIsActive = false;
+      }
     }
   } catch (error) {
     console.log(error.error.message);
